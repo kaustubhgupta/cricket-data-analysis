fc732835e461459564af4a7e329d552966d0619d
kaustubhgupta <kaustubhgupta1828@gmail.com>
2025-03-16 15:41:12 +0530

    added pre check to not drigger dbt in case not new data is added

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -1,11 +1,16 @@
 import os
 from datetime import datetime
 
-from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from airflow.decorators import dag
+from airflow.operators.python import PythonOperator
+
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig, RenderConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+from cosmos.constants import InvocationMode
 
+from utils.pre_check import fetch_pandas_old
 
-profile_config = ProfileConfig(
+profile_config = ProfileConfig( 
     profile_name="cricket_analytics",
     target_name="dev",
     profile_mapping=SnowflakeUserPasswordProfileMapping(
@@ -14,13 +19,36 @@ profile_config = ProfileConfig(
     )
 )
 
-dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/data_pipeline",),
-    operator_args={"install_deps": True },
-    profile_config=profile_config,
-    execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
-    schedule_interval="@daily",
-    start_date=datetime(2023, 9, 10),
+
+@dag(
+    schedule_interval="*/10 * * * *",
+    start_date=datetime(2023, 1, 1),
     catchup=False,
-    dag_id="dbt_dag",
 )
+def main_dag():
+
+    # Define Custom Task (Pre-check)
+    custom_task = PythonOperator(
+        task_id="snowflake_pre_check",
+        python_callable=fetch_pandas_old,
+    )
+
+    # Define DBT Task Group (without dag=dag)
+    dbt_snowflake_dag = DbtTaskGroup(
+        group_id='dbt_task',
+        project_config=ProjectConfig("/usr/local/airflow/dags/dbt/data_pipeline"),
+        operator_args={"install_deps": True},
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",
+            invocation_mode=InvocationMode.SUBPROCESS
+        ),
+        render_config=RenderConfig(
+            enable_mock_profile=False,  # This is necessary to benefit from partial parsing when using ProfileMapping
+        ),
+    )
+
+    # Ensure the custom task runs before DBT
+    custom_task >> dbt_snowflake_dag
+
+main_dag()
---
+++ utils/pre_check.py
@@ -0,0 +1,35 @@
+import snowflake.connector 
+import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = snowflake.connector.connect(
+    user=os.getenv("USER"),
+    password=os.getenv("PASSWORD"),
+    account=os.getenv("ACCOUNT"),
+    warehouse=os.getenv("WAREHOSE"),
+    database=os.getenv("DATABASE"),
+    schema=os.getenv("SCHEMA")
+)
+
+cur = conn.cursor()
+sql = '''
+select * from raw.raw_data where load_date > (select max(etl_load_timestamp) from tranformed_data.stg_raw__cricket_raw_data)
+'''
+
+def fetch_pandas_old():
+    cur.execute(sql)
+    rows = 0
+    while True:
+        dat = cur.fetchmany(50000)
+        if not dat:
+            break
+        df = pd.DataFrame(dat, columns=[desc[0] for desc in cur.description])
+        rows += df.shape[0]
+    
+    if rows == 0:
+        raise ValueError("No new data in raw_data table in Snowflake table, exiting..")  # Raise an exception
+    
+    print(f"Good to go! New matches: {rows}")
